2ed2330d126263376a58638c899b1f0998682a04
Nicolas Guerrero <nicg987@gmail.com>
2014-11-05 12:26:59 -0600

    k

--- KeyTesting.java
+++ KeyTesting.java
@@ -1,26 +1,15 @@
 import java.awt.event.KeyEvent;
+import javax.swing.JFrame;
 
 public class KeyTesting
 {
     public static void main(String[] args) throws Exception
     {
-        System.out.print(" ");
         while (true)
         {
-            int keyPressed = KeyEvent.KEY_TYPED;
-            if (keyPressed != 400)
-            {
-                System.out.print(keyPressed);
-                String key = "" + keyPressed;
-                System.out.print(key);
-                if (key.equals("w"))
-                {
-                    System.out.print("w has been hit");
-                }
-            }
-            
-            
-            Thread.sleep(33);
+            int key = KeyEvent.KEY_TYPED;
+            System.out.print(key);
         }
+        Thread.sleep(100);
     }
 }
