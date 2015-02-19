b043251e23eca1db51f652d96b648958ce4440ee
nfguerrero <nicg987@gmail.com>
2015-02-18 21:18:42 -0600

    stuff?

--- KeyListener Testing/CharacterComponent.java
+++ KeyListener Testing/CharacterComponent.java
@@ -1,3 +1,44 @@
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.awt.event.KeyListener
+import javax.swing.JComponent;
+import java.awt.event.KeyListener;
+import java.awt.event.KeyEvent;
+
+public class CharacterComponent extends JComponent
+{
+    private int x = 0;
+    private int y = 0;
+    
+    public CharacterComponent()
+    {
+        KeyPressListener listener = new KeyPressListener();
+        this.addKeyListener(listener);
+    }
+    
+    public void setDirection(char key)
+    {
+        this.x = 400;
+        this.y = 300;
+        
+        this.repaint();
+    }
+    
+    public void paintComponent(Graphics g)
+    {
+        Graphics2D g2 = (Graphics2D) g;
+        
+        Dot dot = new Dot(this.x, this.y);
+        dot.draw(g2);
+    }
+    
+    public class KeyPressListener implements KeyListener
+    {
+        public void keyTyped(KeyEvent event)
+        {
+            setDirection(event.getKeyChar());
+        }
+        
+        public void keyPressed(KeyEvent event){}
+        public void keyReleased(KeyEvent event){}
+    }
+}
--- KeyListener Testing/CharacterViewer.java
+++ KeyListener Testing/CharacterViewer.java
@@ -7,7 +7,7 @@ public class CharacterViewer
         JFrame frame = new JFrame();
         
         frame.setSize(800, 600);
-        frame.setTitle("Triangle Creator");
+        frame.setTitle("KeyListener Tester");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
         CharacterComponent component = new CharacterComponent();
